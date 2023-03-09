fba341cb57d6a81e16a5a92641e042c93ecf00fd
João Pedro Bergfeld Backer Lopes <113688538+jpedrobacker@users.noreply.github.com>
2023-03-09 16:33:45 -0300

    Add files via upload

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,15 +1,21 @@
 import Jogo
 import random
 
-def jogar():
+def apresentacao():
+    print(" ")
     print("Bem-Vindo ao jogo da adivinhação!")
+    print(" ")
     print("Selecione a dificuldade!")
     print("(1) Fácil (2) Médio (3) Difícil")
 
+def seleciona_nivel():
+    print(" ")
     nivel = int(input("Defina a dificuldade:"))
-
+    print(" ")
     print("Você selecionou a dificuldade {}".format(nivel))
+    return nivel
 
+def tentativas(nivel):
     total_de_tentativas = 0
 
     if  (nivel == 1):
@@ -20,45 +26,66 @@ def jogar():
         
     elif(nivel == 3):
         total_de_tentativas = 3
+    
+    return total_de_tentativas
+
+def reiniciar_parar():
+    print("Deseja jogar novamente ou mudar de jogo?")
+    jogar_novamente = int(input("(1) Jogar nomavamente (2)Mudar de jogo: "))
+            
+    if(jogar_novamente == 1):
+        jogar()
+                
+    elif (jogar_novamente == 2):
+        Jogo.escolha_jogos()
+
+def chute_numero():
+    return int(input("Digite o seu numero entre 1 e 100: "))
+
+def jogar():
+    apresentacao()
+
+    nivel = seleciona_nivel()
+
+    total_de_tentativas = tentativas(nivel)
 
     numero_secreto = random.randrange(1 , 100+1)
-
+    
     for rodada in range (1,total_de_tentativas + 1):
+        print(" ")
         print("Tentativa {} de {}".format(rodada , total_de_tentativas))
-        chute = int(input("Digite o seu numero entre 1 e 100: "))
+        print(" ")
+        chute = chute_numero()
         
         if( chute < 1 or chute > 100):
             print("Você deve digitar um numero entre 1 e 100!")
             continue
-            
+        
+        print(" ")    
         print("Você escolheu:",chute)
         
         acertou = chute == numero_secreto
         maior   = chute >  numero_secreto
         menor   = chute <  numero_secreto
-        errou   = chute != numero_secreto
 
-        if(chute == numero_secreto):
+        if(acertou):
+            print(" ")
             print("Parabéns você acertou o numero secreto")
             break
         else:
             if(maior):
+                print(" ")
                 print("O seu chute foi maior do que o número secreto!")
-            
-            elif(menor):
-                print("O seu chute foi menor do que o número secreto!")
-        
-        if(rodada == total_de_tentativas):
-            print("Seu número de tentativas acabaram, você perdeu, o numero secreto era: {}".format(numero_secreto))
-            print("Deseja jogar novamente ou mudar de jogo?")
-            
-            jogar_novamente = int(input("(1) Jogar nomavamente (2)Mudar de jogo: "))
-            
-            if(jogar_novamente == 1):
-                jogar()
                 
-            elif (jogar_novamente == 2):
-                Jogo.escolha_jogos()
+            elif(menor):
+                print(" ")
+                print("O seu chute foi menor do que o número secreto!")
+
+        if(rodada == total_de_tentativas):
+            print(" ")
+            print("Seu número de tentativas acabaram, você perdeu, o numero secreto era: {}".format(numero_secreto))
+            print(" ")
+            reiniciar_parar()
 
 if(__name__ == "__main__"):
     jogar()
--- Forca.py
+++ Forca.py
@@ -1,10 +1,47 @@
 import Jogo
+import random
 
-def jogar():
-    print("Bem-Vindo ao jogo da Forca")
+def carrega_palavra_secreta():
+    with open('palavras.txt') as arquivo:
+        palavras = [linha.strip() for linha in arquivo]
+        
+    arquivo.closed
+    
+    n = random.randrange(0,len(palavras))
 
+    palavra_secreta = palavras[n].upper()
+    return palavra_secreta
 
+def iniaciliza_lista(palavra_secreta):
+    lista = ["_"]*len(palavra_secreta)
+    return lista
 
+def pede_chute():
+    chute = input("Digite uma letra: ")
+    chute = chute.strip().upper()
+    return chute
+
+def chute_certo(palavra_secreta, chute, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if(chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
+
+def ganhou(letras_acertadas):
+    print(letras_acertadas)
+    print("Fim de Jogo, você ganhou!")
+
+def perdeu (letras_acertadas):
+    print(letras_acertadas)
+    print("Forca, você perdeu!")
+
+def chute_errado(erros, letras_erradas, chute):
+    letras_erradas.append(chute) 
+    print("Você errou, {} tentativa(s) de 6. Letras erradas:{}".format(erros , letras_erradas))
+    return letras_erradas
+
+def reiniciar_parar():
     print("Deseja jogar novamente ou mudar de jogo?")
     jogar_novamente = int(input("(1) Jogar nomavamente (2)Mudar de jogo: "))
             
@@ -13,6 +50,43 @@ def jogar():
                 
     elif (jogar_novamente == 2):
         Jogo.escolha_jogos()
+
+def jogar():
+    
+    print("Bem-Vindo ao jogo da Forca")
+    
+    palavra_secreta = carrega_palavra_secreta()
+    
+    letras_acertadas = iniaciliza_lista(palavra_secreta)
+    
+    acertou  = False
+    enforcou = False
+    erros = 0
+    letras_erradas = []
+
+    while (not enforcou and not acertou):
+        
+        print(letras_acertadas)
+        chute = pede_chute()
+        
+        if (chute in palavra_secreta):
+            print("Letras erradas:",letras_erradas)
+            
+            chute_certo(palavra_secreta,chute,letras_acertadas)
+                
+        else:
+            erros += 1
+            chute_errado(erros,letras_erradas, chute)
+                
+        if (erros == 6):
+            perdeu(letras_acertadas)
+            break
+        
+        elif ("_" not in letras_acertadas):
+            ganhou(letras_acertadas)
+            break
+
+    reiniciar_parar()
 
 if(__name__ == "__main__"):
     jogar()
